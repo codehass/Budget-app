80c0c629759cd8d9c1cf410a127454733ce33540
codehass <hassan.elouardy06@gmail.com>
2023-04-24 19:54:25 +0000

    Migrate panding migrates

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_24_142647) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_24_184405) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "entities", force: :cascade do |t|
+    t.string "author"
+    t.string "name"
+    t.integer "amount"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_entities_on_user_id"
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "purchases", force: :cascade do |t|
+    t.bigint "group_id", null: false
+    t.bigint "entity_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["entity_id"], name: "index_purchases_on_entity_id"
+    t.index ["group_id"], name: "index_purchases_on_group_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_24_142647) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "entities", "users"
+  add_foreign_key "purchases", "entities"
+  add_foreign_key "purchases", "groups"
 end
