34568c49cf1a03526c97c00a271f84d2c4f11edf
codehass <hassan.elouardy06@gmail.com>
2023-04-24 20:21:26 +0000

    Add relationship

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,5 @@
 class Group < ApplicationRecord
+  belongs_to :user
+  has_many :purchases, foreign_key: 'group_id', dependent: :delete_all
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
+
+  has_many :groups, foreign_key: 'author_id', dependent: :delete_all
+  has_many :entities, foreign_key: 'author_id', dependent: :delete_all
 end
