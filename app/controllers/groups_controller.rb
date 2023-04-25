ade8b49986464336d05f3ece3f77afd5386eb688
codehass <hassan.elouardy06@gmail.com>
2023-04-25 17:07:51 +0000

    Edit group controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,10 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: %i[ show edit update destroy ]
-
+  load_and_authorize_resource
   # GET /groups or /groups.json
   def index
-    @groups = Group.all
+    @groups = Group.includes(:entities).where(user_id: current_user.id)
+    @groups = Group.order(created_at: :desc)
   end
 
   # GET /groups/1 or /groups/1.json
@@ -22,6 +23,7 @@ class GroupsController < ApplicationController
   # POST /groups or /groups.json
   def create
     @group = Group.new(group_params)
+    @group.user_id = current_user.id
 
     respond_to do |format|
       if @group.save
