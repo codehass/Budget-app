3b7eb1347bd1c418044c7a9dbabfbe3aca503d23
codehass <hassan.elouardy06@gmail.com>
2023-04-25 17:06:11 +0000

    Generate migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_24_184405) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_25_132703) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_24_184405) do
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
   end
 
   create_table "purchases", force: :cascade do |t|
