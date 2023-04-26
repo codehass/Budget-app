21d5c8afe377b7005fd593077b1b0dd6a85fdc5c
codehass <hassan.elouardy06@gmail.com>
2023-04-26 00:34:53 +0000

    Edit entity relationship

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,4 +1,4 @@
 class Entity < ApplicationRecord
-  belongs_to :user, foreign_key: 'user_id'
+  belongs_to :user, class_name: 'User'
   has_many :groups, foreign_key: 'entity_id', dependent: :delete_all
 end
