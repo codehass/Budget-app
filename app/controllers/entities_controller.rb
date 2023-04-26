5f3e19b24bdf5c1d9f56c22753713b8e700c27e8
codehass <hassan.elouardy06@gmail.com>
2023-04-26 18:58:30 +0000

    Make changes entities controller

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,39 +1,29 @@
 class EntitiesController < ApplicationController
-  load_and_authorize_resource
+  before_action :authenticate_user!
 
-  # GET /entities or /entities.json
-  def index
-    @entities = Entity.where(user_id: current_user.id)
-  end
-
-  # GET /entities/new
   def new
     @entity = Entity.new
-    @groups = Group.all
     @purchases = Purchase.new
+    @groups = Group.where(user_id: current_user.id)
   end
 
-  # POST /entities or /entities.json
   def create
-    @entity = Entity.new(name: entity_params[:name], author: entity_params[:author], amount: entity_params[:amount])
-    @entity.author = current_user.name
-    @entity.user_id = current_user.id 
-    
+    @entity = Entity.new(
+      name: permitted[:name], user_id: permitted[:user_id], amount: permitted[:amount])
+      @entity.user_id = current_user.id
     if @entity.save
-      @purchase = Purchase.create(group_id: entity_params[:group_id], entity_id: @entity.id)
-      flash[:notice] = 'Purchase created successfully'
-
-      redirect_to group_path(current_user, entity_params[:group_id])
+      @purchase = Purchase.create(group_id: permitted[:group_id], entity_id: @entity.id)
+      flash[:notice] = 'Transaction created successfully'
+      redirect_to user_group_path(current_user, permitted[:group_id])
     else
       flash[:alert] = 'Error: Could not create a transaction'
       redirect_to new_entity_path
     end
-
   end
 
   private
-    # Only allow a list of trusted parameters through.
-    def entity_params
-      params.require(:entity).permit(:author, :name, :amount, :user_id)
-    end
+
+  def permitted
+    params.require(:entity).permit(:user_id, :name, :amount, :group_id)
+  end
 end
