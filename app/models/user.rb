5180bad0ba50cfb58a23b0a1c9e08a98bd9d14cb
codehass <hassan.elouardy06@gmail.com>
2023-04-27 16:50:19 +0000

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
 
   has_many :groups, foreign_key: 'user_id', dependent: :delete_all
   has_many :entities, foreign_key: 'user_id', dependent: :delete_all
+  validates :name, presence: true
 end
