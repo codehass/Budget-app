a4ab03b8122a0633e405ed6ea0121fdc58c2728f
codehass <hassan.elouardy06@gmail.com>
2023-04-27 12:20:05 +0000

    fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :groups, foreign_key: 'user_id', dependent: :delete_all
