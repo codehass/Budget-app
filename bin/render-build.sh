bd8711f33636111df348d746245630571eed6cb4
codehass <hassan.elouardy06@gmail.com>
2023-04-28 09:10:50 +0000

    Deploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,8 @@
-#!/user/bin/env bash
+#!/usr/bin/env bash
 # exit on error
 set -o errexit
 
 bundle install
-bundle exex rails assets:precompile
-bundle exec rails assets:clean
-bundle exec rails db:migrate
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
