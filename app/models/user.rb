1a8259153b0d91626b21af2602878873e2acd632
codehass <hassan.elouardy06@gmail.com>
2023-04-28 10:11:08 +0000

    Update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   has_many :groups, foreign_key: 'user_id', dependent: :delete_all
   has_many :entities, foreign_key: 'user_id', dependent: :delete_all
